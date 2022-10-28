0a580cfade8218322825f23b2cf7129f0d785071
ayushakash <ayushakash9@gmai.com>
2022-10-28 21:46:11 +0530

    Project initialised

---
+++ src/Components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <>
+    <div className='d-flex justify-content-between align-items-center'>
+
+    <div className='mx-5'><h1 >Get Instant Cash Loan Up to ₹20 lakh with Cashier</h1> </div>
+
+
+    <div> bike</div>
+
+    </div>
+      
+    </>
+  )
+}
+
+export default Main
